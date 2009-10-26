791b1f887114b901c4d5c920a6e9d35069c7271c
Julio Monteiro <julio@monteiro.eti.br>
2009-10-27 06:36:52 +0800

    Changing last hon-stats to honstats

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'hon-stats'
+require 'honstats'
 require 'spec'
 require 'spec/autorun'
 
