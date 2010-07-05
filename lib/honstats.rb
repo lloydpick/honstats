2d38128820eec1df10e89b40e69ebcbd3d4deae1
Lloyd Pick <lloydpick@gmail.com>
2010-07-05 18:05:38 +0100

    updated the dependencies and fixed the loading order of the team modules

--- lib/honstats.rb
+++ lib/honstats.rb
@@ -1,17 +1,20 @@
 require 'rubygems'
 
-gem 'libxml-ruby', '>= 0.8.3'
+gem 'libxml-ruby', '>= 1.1.4'
 require 'xml'
 
-gem 'eventmachine', '>= 0.12.8'
+gem 'eventmachine', '>= 0.12.10'
 require 'eventmachine'
 include EM::Deferrable
 
-require 'activesupport'
+require 'active_support'
 require 'uri'
 require 'cgi'
 require 'fileutils'
 
 require "#{File.dirname(__FILE__)}/honstats/base.rb"
 Dir["#{File.dirname(__FILE__)}/honstats/*.rb"].each { |source_file| require source_file }
-Dir["#{File.dirname(__FILE__)}/honstats/*/*.rb"].each { |source_file| require source_file }
+Dir["#{File.dirname(__FILE__)}/honstats/clan/*.rb"].each { |source_file| require source_file }
+require "#{File.dirname(__FILE__)}/honstats/match/team.rb"
+require "#{File.dirname(__FILE__)}/honstats/match/team_player.rb"
+require "#{File.dirname(__FILE__)}/honstats/match/team_stat.rb"
